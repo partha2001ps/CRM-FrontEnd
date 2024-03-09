83924b6c6e8524bd84422c87851a4826f5a8a000
partha2001ps <parthapn2017@gmail.com>
2024-03-09 13:38:01 +0530

    All Done

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import {  Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Login from './components/login';
+import PasswordReset from './components/passwordreset';
+import NewPassword from './components/newpassword';
+import Active from './components/active';
+import Products from './components/products';
+import Ticket from './components/Ticket';
+import CreateTicket from './components/CreateTicket';
+import Profile from './components/Dashboard';
+import Admin from './components/Admin';
+import AssignTickets from './components/MyTickets';
+import CreateProduct from './components/CreateProduct';
+
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <div>
+          <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/reset-password' element={<PasswordReset />} />
+            <Route path='/reset-password/new-password/:OTP' element={<NewPassword />} />
+            <Route path='/activate-account/:activationToken' element={<Active />} />
+            <Route path='/dashboard' element={<Profile />} />
+            <Route path='/ticket' element={<Ticket />} />
+            <Route path='/create' element={<CreateTicket />} />
+            <Route path='/products' element={<Products />} />
+            <Route path='/admin' element={<Admin />} />
+            <Route path='/mytickets' element={<AssignTickets />} />
+            <Route path='/create-product' element={ <CreateProduct/>} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
